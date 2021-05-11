8a1c9b2ffd609d8975638725f3b84d505572db70
justin-hill426 <jrhill426@gmail.com>
2021-05-11 10:50:47 -0700

    Fixed Search Functionality

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ FSJS Project 2 - Data Pagination and Filtering
 
 const STUDENTS_PER_PAGE = 9;
 
+
+
 /*
 Extra Credit
 */
@@ -21,22 +23,23 @@ function createSearchBar() {
    studentsH2.insertAdjacentHTML('afterend',searchBarHTML);
 }
 
-function addSearchFunctionality(list) {
-  const studentSearch = document.querySelector('.student-search');
-  studentSearch.addEventListener('keyup', (e) => {
-    document.querySelector('.student-list') = "";
-      const search = document.querySelector("#search");
-      let searchList = [];
-      for(let i = 0; i < list.length; i++) {
-        const fullName = list[i].name.first + " " + list[i].name.last;
-        if(fullName.toLowerCase().includes(search.value.toLowerCase()) && search.value.length !== 0) {
-          searchList.push(list[i]);
-        }
-      }
-      showPage(searchList, 1);
-  });
-}
+function addSearchFunctionality(searchInput, list) {
+  let searchList = [];
+  for(let i = 0; i < list.length; i++) {
+    const fullName = list[i].name.first + " " + list[i].name.last;
+    if(fullName.toLowerCase().includes(searchInput.toLowerCase()) && searchInput.length !== 0) {
+      searchList.push(list[i]);
+    }
+  }
+  if(searchInput.length == 0) {
+    showPage(data, 1);
+  }
+  else {
+    showPage(searchList, 1);
+    addPagination(searchList);
+  }
 
+}
 
 
 /*
@@ -67,21 +70,23 @@ function showPage(list, page) {
   };
 
   for(let i = startIndex; i < endIndex; i++) {
-    const li = createElement('li', 'student-item cf');
-    const studentInfoDiv = createElement('div', 'student-details');
-    const img = createElement('img', 'avatar', data[i].picture.large);
-    let studentName = data[i].name.first + " " + data[i].name.last;
-    const h3 = createElement('h3', '', studentName);
-    const emailSpan = createElement('span', 'email', data[i].email);
-    studentInfoDiv.appendChild(img);
-    studentInfoDiv.appendChild(h3);
-    studentInfoDiv.appendChild(emailSpan);
-    const joinedDiv = createElement('div', 'joined-details');
-    const joinedSpan = createElement('span', 'date', "Joined " + data[i].registered.date);
-    joinedDiv.appendChild(joinedSpan)
-    li.appendChild(studentInfoDiv);
-    li.appendChild(joinedDiv);
-    studentList.appendChild(li);
+    if(list[i]) {
+      const li = createElement('li', 'student-item cf');
+      const studentInfoDiv = createElement('div', 'student-details');
+      const img = createElement('img', 'avatar', list[i].picture.large);
+      let studentName = list[i].name.first + " " + list[i].name.last;
+      const h3 = createElement('h3', '', studentName);
+      const emailSpan = createElement('span', 'email', list[i].email);
+      studentInfoDiv.appendChild(img);
+      studentInfoDiv.appendChild(h3);
+      studentInfoDiv.appendChild(emailSpan);
+      const joinedDiv = createElement('div', 'joined-details');
+      const joinedSpan = createElement('span', 'date', "Joined " + list[i].registered.date);
+      joinedDiv.appendChild(joinedSpan)
+      li.appendChild(studentInfoDiv);
+      li.appendChild(joinedDiv);
+      studentList.appendChild(li);
+    }
   }
 }
 
@@ -105,7 +110,9 @@ function addPagination(list) {
     linkList.insertAdjacentHTML('beforeend', buttonStr);
   }
   const actButton = linkList.querySelector('button:first-child');
-  actButton.className = 'active';
+  if(actButton) {
+    actButton.className = 'active';
+  }
   linkList.addEventListener('click', (e) => {
     if(e.target.tagName === 'BUTTON') {
       const activeButton = linkList.querySelector('button.active');
@@ -120,6 +127,16 @@ function addPagination(list) {
 
 // Call functions
 createSearchBar();
+const search = document.querySelector('.student-search');
+const searchSubmit = document.querySelector('label button');
 showPage(data, 1);
 addPagination(data);
-addSearchFunctionality(data);
+
+searchSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  addSearchFunctionality(search.target.value, data);
+});
+
+search.addEventListener('keyup', (event) => {
+  addSearchFunctionality(event.target.value, data);
+});
